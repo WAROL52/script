4a9eb1639c0224672dd2dd8f5427d6c565fc0c68
WAROL52 <rabetsyrolio@gmail.com>
2024-09-16 09:20:46 +0300

    mise a jour de push swap tester

--- 42/push_swap/test.sh
+++ 42/push_swap/test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "test a faire..."
+
+if [ -f ./checker_linux ]; then
+    echo -e "./checker_linux OK"
+else
+    wget https://cdn.intra.42.fr/document/document/27680/checker_linux
+    chmod 777 ./checker_linux
+fi
+
+curl https://raw.githubusercontent.com/hu8813/tester_push_swap/main/pstester.py | python3 -
